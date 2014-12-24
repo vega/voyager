7cc8bf1b0b0bdcb107554f77041b522445f05786
Dominik Moritz <domoritz@gmail.com>
2014-12-24 10:45:32 -0800

    Run tests in chrome

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -44,12 +44,14 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows)
     browsers: [
-      'PhantomJS'
+      'PhantomJS',
+      'Chrome'
     ],
 
     // Which plugins to enable
     plugins: [
       'karma-phantomjs-launcher',
+      'karma-chrome-launcher',
       'karma-jasmine'
     ],
 
