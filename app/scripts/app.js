f02f725a6444a89498cf4bbc7b73c55cf16b97ce
Dominik Moritz <domoritz@gmail.com>
2014-12-24 10:44:15 -0800

    Initial commit of yeoman angular app

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name uiApp
+ * @description
+ * # uiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('uiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the uiApp
+ */
+angular.module('uiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
