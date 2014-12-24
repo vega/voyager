72ad6e9c582d8ee77e3ea75c47417ad350fa515d
Dominik Moritz <domoritz@gmail.com>
2014-12-24 11:18:19 -0800

    Remove deprecated grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -403,11 +403,6 @@ module.exports = function (grunt) {
     ]);
   });
 
-  grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
-    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    grunt.task.run(['serve:' + target]);
-  });
-
   grunt.registerTask('test', [
     'clean:server',
     'concurrent:test',
