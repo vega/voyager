958d2d6186b3f6e5978de06200e13ecbaadded92
Dominik Moritz <domoritz@gmail.com>
2014-12-28 23:29:19 -0800

    We need to make copies of mark config objects

--- app/scripts/services/marks.js
+++ app/scripts/services/marks.js
@@ -2,123 +2,137 @@
 
 angular.module('vleApp')
   .constant('Marks', function() {
-  	var includeZero = {
-		name: 'zero',
-		default: true,
-		type: 'bool',
-		property: 'scale'
-	}
+  	var includeZero = function() {
+  		return {
+			name: 'zero',
+			default: true,
+			type: 'bool',
+			property: 'scale'
+		}
+	};
 
-	var reverse = {
-		name: 'reverse',
-		default: false,
-		type: 'bool',
-		property: 'scale'
-	}
+	var reverse = function() {
+  		return {
+			name: 'reverse',
+			default: false,
+			type: 'bool',
+			property: 'scale'
+		}
+	};
 
-	var type = {
-		name: 'type',
-		default: 'linear',
-		type: 'choice',
-		choices: ['linear', 'logarithmic', 'power', 'quantile'],
-		property: 'scale'
-	}
+	var type = function() {
+  		return {
+			name: 'type',
+			default: 'linear',
+			type: 'choice',
+			choices: ['linear', 'logarithmic', 'power', 'quantile'],
+			property: 'scale'
+		}
+	};
 
-	var margin = {
-		name: 'margin',
-		default: 80,
-		type: 'integer',
-		property: 'axis'
-	}
+	var margin = function() {
+  		return {
+			name: 'margin',
+			default: 80,
+			type: 'integer',
+			property: 'axis'
+		}
+	};
 
-	var weight = {
-		name: 'weight',
-		default: 'normal',
-		type: 'choice',
-		choices: ['normal', 'bold'],
-		property: 'text'
-	}
+	var weight = function() {
+  		return {
+			name: 'weight',
+			default: 'normal',
+			type: 'choice',
+			choices: ['normal', 'bold'],
+			property: 'text'
+		}
+	};
 
-	var size = {
-		name: 'size',
-		default: 10,
-		type: 'integer',
-		property: 'font'
-	}
+	var size = function() {
+  		return {
+			name: 'size',
+			default: 10,
+			type: 'integer',
+			property: 'font'
+		}
+	};
 
-	var font = {
-		name: 'name',
-		default: 'Halvetica Neue',
-		type: 'string',
-		property: 'font'
-	}
+	var font = function() {
+  		return {
+			name: 'name',
+			default: 'Halvetica Neue',
+			type: 'string',
+			property: 'font'
+		}
+	};
 
 	var marks = [
 		{
 			name: 'x',
 			config: {
-				Q: [includeZero,
-					reverse,
-					type,
-					margin],
-				O: [reverse,
-					margin],
-				T: [reverse,
-					type,
-					margin]
+				Q: [includeZero(),
+					reverse(),
+					type(),
+					margin()],
+				O: [reverse(),
+					margin()],
+				T: [reverse(),
+					type(),
+					margin()]
 			}
 		},{
 			name: 'y',
 			config: {
-				Q: [includeZero,
-					reverse,
-					type,
-					margin],
-				O: [reverse,
-					margin],
-				T: [reverse,
-					type,
-					margin]
+				Q: [includeZero(),
+					reverse(),
+					type(),
+					margin()],
+				O: [reverse(),
+					margin()],
+				T: [reverse(),
+					type(),
+					margin()]
 			}
 		},{
 			name: 'row',
 			config: {
-				O: [reverse]
+				O: [reverse()]
 			}
 		},{
 			name: 'col',
 			config: {
-				O: [reverse]
+				O: [reverse()]
 			}
 		},{
 			name: 'size',
 			config: {
-				Q: [includeZero,
-					reverse,
-					type],
-				O: [reverse],
-				T: [reverse,
-					type]
+				Q: [includeZero(),
+					reverse(),
+					type()],
+				O: [reverse()],
+				T: [reverse(),
+					type()]
 			}
 		},{
 			name: 'color',
 			config: {
-				Q: [includeZero,
-					reverse,
-					type],
-				O: [reverse],
-				T: [reverse,
-					type]
+				Q: [includeZero(),
+					reverse(),
+					type()],
+				O: [reverse()],
+				T: [reverse(),
+					type()]
 			}
 		},{
 			name: 'alpha',
 			config: {
-				Q: [includeZero,
-					reverse,
-					type],
-				O: [reverse],
-				T: [reverse,
-					type]
+				Q: [includeZero(),
+					reverse(),
+					type()],
+				O: [reverse()],
+				T: [reverse(),
+					type()]
 			}
 		},{
 			name: 'shape',
@@ -128,9 +142,9 @@ angular.module('vleApp')
 		},{
 			name: 'text',
 			config: {
-				Q: [weight, size, font],
-				O: [weight, size, font],
-				T: [weight, size, font]
+				Q: [weight(), size(), font()],
+				O: [weight(), size(), font()],
+				T: [weight(), size(), font()]
 			}
 		}
 	]
