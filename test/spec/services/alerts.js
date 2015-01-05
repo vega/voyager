b0808c72ace42b38e88d9f213b0cb13681c94484
Dominik Moritz <domoritz@gmail.com>
2015-01-04 20:34:16 -0800

    Test that alerts close themselves if dismiss is set

--- test/spec/services/alerts.js
+++ test/spec/services/alerts.js
@@ -6,9 +6,10 @@ describe('Service: Alerts', function () {
   beforeEach(module('vleApp'));
 
   // instantiate service
-  var alerts;
-  beforeEach(inject(function (_Alerts_) {
+  var alerts, $timeout;
+  beforeEach(inject(function (_Alerts_, _$timeout_) {
     alerts = _Alerts_;
+    $timeout = _$timeout_;
   }));
 
   it('should add alerts', function () {
@@ -19,6 +20,14 @@ describe('Service: Alerts', function () {
     expect(alerts.alerts).toEqual([{msg: 'foo'}, {msg: 'bar'}]);
     alerts.closeAlert(0);
     expect(alerts.alerts).toEqual([{msg: 'bar'}]);
+  });
+
+  it('alerts should close themselves', function () {
+    expect(alerts.alerts).toEqual([]);
+    alerts.add('foo', 100);
+    expect(alerts.alerts).toEqual([{msg: 'foo'}]);
+    $timeout.flush();
+    expect(alerts.alerts).toEqual([]);
   });
 
 });
