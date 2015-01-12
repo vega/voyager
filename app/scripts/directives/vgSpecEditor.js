16a9ec5d8adab9b77584cb41a4cf3ddc1474460d
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-01-11 22:17:51 -0800

    parse Vegalite service into vl/vgSpecEditor

--- app/scripts/directives/vgSpecEditor.js
+++ app/scripts/directives/vgSpecEditor.js
@@ -7,17 +7,7 @@ angular.module('vleApp')
       restrict: 'E',
       scope: {},
       link: function postLink(scope, element, attrs) {
-        // TODO(kanitw): I don't know how expensive is this "function" watcher.
-        // should we use some event listener instead?
-        scope.$watch(
-          function(){ return Vegalite.vegaSpec; },
-          function(newSpec) {
-            if (!newSpec) {
-              return;
-            }
-            scope.vgSpec = Vegalite.vegaSpec;
-          }
-        );
+        scope.Vegalite = Vegalite;
       }
     };
   });
--- app/scripts/directives/vlSpecEditor.js
+++ app/scripts/directives/vlSpecEditor.js
@@ -7,24 +7,11 @@ angular.module('vleApp')
       restrict: 'E',
       scope: {},
       link: function postLink(scope, element, attrs) {
+        scope.Vegalite = Vegalite;
 
         scope.parseShorthand = function(shorthand) {
           VegaliteSpec.parseShorthand(shorthand);
         };
-
-        // TODO(kanitw): I don't know how expensive is this "function" watcher.
-        // should we use some event listener instead?
-        scope.$watch(
-          function(){ return Vegalite.vegaSpec; },
-          function(newSpec) {
-            if (!newSpec) {
-              return;
-            }
-            scope.vlSpec = Vegalite.vlSpec;
-            scope.shorthand = Vegalite.shorthand;
-          }
-        );
-
       }
     };
   });
