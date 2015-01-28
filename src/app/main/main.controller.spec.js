6fdffcc66e90768c77e9e5964a8c4789e7581a52
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-01-27 22:52:18 -0800

    update main, remove useless tests

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -8,15 +8,4 @@ describe('controllers', function(){
   beforeEach(inject(function($rootScope) {
     scope = $rootScope.$new();
   }));
-
-  it('should define more than 5 awesome things', inject(function($controller) {
-    expect(scope.awesomeThings).toBeUndefined();
-
-    $controller('MainCtrl', {
-      $scope: scope
-    });
-
-    expect(angular.isArray(scope.awesomeThings)).toBeTruthy();
-    expect(scope.awesomeThings.length > 5).toBeTruthy();
-  }));
 });
