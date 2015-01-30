f745e70dba34de1d7b3519f8ce2efc19f550b80e
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-01-30 07:14:19 -0800

    show nice vis in the visList

--- src/app/visrec/visrec.service.js
+++ src/app/visrec/visrec.service.js
@@ -8,7 +8,7 @@
  * Service in the facetedviz.
  */
 angular.module('facetedviz')
-  .service('Visrec', function (vr, vl, _) {
+  .service('Visrec', function (vr, vl, _, Config, Dataset) {
     var Visrec = {
       // projections = [ fieldSet ]
       projections: [],
@@ -16,18 +16,31 @@ angular.module('facetedviz')
       aggregates: {},
       // chartClusters[pkey][akey]
       chartClusters: {},
-      opt: {},
       update: {}
     };
 
+    // Visrec Config
+
+    Visrec.opt = {
+      genAggr: true,
+      genBin: true,
+      genTypeCasting: true,
+      omitTranpose: false,
+      omitDotPlotWithExtraEncoding: false,
+      omitAggrWithAllDimsOnFacets: false,
+      omitDimensionOnly: true,
+      omitAggregateWithMeasureOnly: true
+    };
+
+
     Visrec.update.projections = function(fieldList) {
       // TODO decide if we can update projections only if field name list changes
-      Visrec.projections = vr.gen.projections(fieldList);
+      Visrec.projections = vr.gen.projections(fieldList, Visrec.opt);
       // console.log('projections', Visrec.projections);
 
       Visrec.aggregates = Visrec.projections.reduce(function(a, p) {
         var key = p.key;
-        a[key] = vr.gen.aggregates([], p);
+        a[key] = vr.gen.aggregates([], p, Visrec.opt);
         return a;
       }, {});
       // console.log('aggregates', Visrec.aggregates);
@@ -35,8 +48,34 @@ angular.module('facetedviz')
       Visrec.chartClusters = _.reduce(Visrec.aggregates, function(c, fieldSets, pkey) {
 
         c[pkey] = fieldSets.reduce(function(cp, fieldSet) {
-          cp[fieldSet.key] = vr.gen.encodings([], fieldSet, Visrec.opt, /*cfg*/ {});
-          // console.log('pkey:', pkey, 'akey:', fieldSet.key);
+
+          // FIXME restructure this part of code
+
+          var encodings = vr.gen.encodings([], fieldSet, Visrec.opt, Config.config);
+
+          var clusterIndices = vr.cluster(encodings, 2.5)
+            .map(function (cluster) {
+              // rank item in each cluster
+              return cluster.sort(function (i, j) {
+                return encodings[j].score - encodings[i].score;
+              });
+            })
+            .sort(function (c1, c2) {
+              // rank cluster by top item in each cluster
+              return encodings[c2[0]].score - encodings[c1[0]].score;
+            });
+
+          cp[fieldSet.key] =  clusterIndices.map(function(indices) {
+            return indices.map(function(index) {
+              var spec = encodings[index],
+                encoding = vl.Encoding.fromSpec(spec);
+
+              return {
+                vlSpec: spec,
+                vgSpec: vl.compile(encoding, Dataset.stats)
+              };
+            });
+          });
           return cp;
         }, {});
 
