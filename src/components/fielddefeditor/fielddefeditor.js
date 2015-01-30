9c14c53c7310fbc118ef2367394975d3f352cb4b
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-01-30 14:47:05 -0800

    only allow sensible type casting

--- src/components/fielddefeditor/fielddefeditor.js
+++ src/components/fielddefeditor/fielddefeditor.js
@@ -2,6 +2,8 @@
 
 angular.module('vleApp')
   .directive('fieldDefEditor', function(Dataset, _) {
+
+
     return {
       templateUrl: 'components/fielddefeditor/fielddefeditor.html',
       restrict: 'E',
@@ -15,6 +17,14 @@ angular.module('vleApp')
         scope.propsExpanded = false;
         scope.funcsExpanded = false;
 
+        scope.allowedCasting = {
+          Q: ['Q', 'O', 'T'],
+          O: ['O'],
+          T: ['T', 'O'],
+          G: ['G', 'O']
+        };
+
+        scope.Dataset = Dataset;
         scope.typeNames = Dataset.typeNames;
 
         function fieldPill(){
@@ -63,6 +73,10 @@ angular.module('vleApp')
             }
           }, true);
         });
+
+        scope.$watch('allowedCasting[Dataset.dataschema.byName[enc[encType].name].type]', function(allowedTypes){
+          scope.allowedTypes = allowedTypes;
+        })
 
 
       }
