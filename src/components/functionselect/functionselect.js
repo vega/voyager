c55b4d475dbc623867a9dd58eff69c7ba191ba48
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-01-31 15:30:52 -0800

    simplify functionselect

--- src/components/functionselect/functionselect.js
+++ src/components/functionselect/functionselect.js
@@ -11,113 +11,69 @@ angular.module('vleApp')
         schema: '='
       },
       link: function(scope /*,element, attrs*/) {
-        scope.selectedAggFunc = {name: ''};
+        var BIN='bin', RAW='';
 
-        function fieldPill(){
+        scope.func = {
+          selected: RAW,
+          list: [RAW]
+        };
+
+        function fieldPill() {
           return scope.pills ? scope.pills[scope.encType] : null;
         }
 
+        function getFns(type) {
+          var schema = scope.schema.properties;
+          return schema.fn && (!schema.fn.supportedTypes || schema.fn.supportedTypes[type]) ? schema.fn.enum : [];
+        }
+
+        function getAggrs(type) {
+          var schema = scope.schema.properties;
+          if (schema.aggr && (!schema.aggr.supportedTypes || schema.aggr.supportedTypes[type])){
+            return schema.aggr.supportedEnums ? schema.aggr.supportedEnums[type] : schema.aggr.enum;
+          }
+          return [];
+        }
+
         // when the function select is updated, propagates change the parent
-        scope.$watch('selectedAggFunc', function(aggFunc) {
+        scope.$watch('func.selected', function(selectedFunc) {
           var oldPill = fieldPill(),
-            pill = _.clone(oldPill);
+            pill = _.clone(oldPill),
+            type = pill ? pill.type : '';
 
           if(!pill){
             return; // not ready
           }
 
           // reset field def
-          pill.bin = undefined;
-          pill.aggr = undefined;
-          pill.fn = undefined;
+          pill.bin = selectedFunc === BIN ? true : undefined;
+          pill.aggr = getAggrs(type).indexOf(selectedFunc) !== -1 ? selectedFunc : undefined;
+          pill.fn = getFns(type).indexOf(selectedFunc) !== -1 ? selectedFunc : undefined;
 
-          var group = aggFunc.group,
-            name = aggFunc.name;
-
-          if (group === 'Binning') {
-            pill.bin = true;
-          } else if (group === 'Aggregation') {
-            pill.aggr = name;
-          } else if (group === 'Function') {
-            pill.fn = name;
-          }
           if(!_.isEqual(oldPill, pill)){
             scope.pills[scope.encType] = pill;
-            // console.log('selectedAggFunc updated', pill);
             scope.pills.update(scope.encType);
           }
         });
 
         // when parent objects modify the pill
-        scope.$watch('pills[encType]', pillUpdated, true);
-
-        function pillUpdated() {
+        scope.$watch('pills[encType]', function (pill, oldPill) {
           // only run this if schema is not null
-          var pill = fieldPill();
-
           if (!scope.schema || !pill) {
-            // console.log('schema', scope.schema, 'pill', pill, 'pills', scope.pills, 'encType', scope.encType);
             return;
           }
 
           var type = pill.name ? pill.type : '';
           var schema = scope.schema.properties;
+          if(!oldPill || pill.type !== oldPill.type){
+            scope.func.list = [''].concat(getFns(type))
+              .concat(getAggrs(type))
+              .concat(schema.bin && schema.bin.supportedTypes[type] ? ['bin'] : []);
 
-          var functions = [{name: ''}], selectedAggFunc={name:''};
-
-          if (schema.fn && (!schema.fn.supportedTypes || schema.fn.supportedTypes[type])) {
-            var fns = schema.fn.enum;
-            fns.forEach(function(fn) {
-              functions.push({
-                'name': fn,
-                'group': 'Function'
-              });
-            });
-
-            if (pill.fn) {
-              selectedAggFunc = {name: pill.fn, group: 'Aggregation'};
-            }
           }
-
-          // set aggregation functions
-          if (schema.aggr && (!schema.aggr.supportedTypes || schema.aggr.supportedTypes[type])) {
-            var aggFunctions = schema.aggr.supportedEnums ? schema.aggr.supportedEnums[type] : schema.aggr.enum;
-
-            _.each(aggFunctions, function(aggFunc) {
-              functions.push({
-                name: aggFunc,
-                group: 'Aggregation'
-              });
-
-            });
-
-            if (pill.aggr) {
-              selectedAggFunc = {name: pill.aggr, group: 'Aggregation'};
-            }
-          }
-
-          // add binning function
-          if (schema.bin && schema.bin.supportedTypes[type]) {
-            var bin = {
-              name: 'bin',
-              group: 'Binning'
-            };
-            functions.push(bin);
-
-            if (pill.bin === true) {
-              selectedAggFunc = bin;
-            }
-          }
-
-          // omit nulls
-          functions = _.filter(functions, function(f) {
-            return f.name !== null;
-          });
-
-          scope.functions = functions;
-          // console.log('pillUpdated', pill, selectedAggFunc);
-          scope.selectedAggFunc = selectedAggFunc;
-        }
+          scope.func.selected = pill.bin ? 'bin' :
+            pill.aggr || pill.fn || RAW;
+        }, true);
       }
     };
   });
