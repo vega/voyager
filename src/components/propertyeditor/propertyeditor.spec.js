de33c6ba1fddb40c7288e2406702fbd04fcbede0
Dominik Moritz <domoritz@gmail.com>
2015-02-06 15:47:13 -0800

    Add a property editor directive

---
+++ src/components/propertyeditor/propertyeditor.spec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+describe('Directive: propertyEditor', function () {
+
+  // load the directive's module
+  beforeEach(module('vleApp'));
+
+  var element,
+    scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+
+    scope.id = "foo";
+    scope.type = "boolean";
+    scope.name = "bar";
+    scope.enum = undefined;
+    scope.group = {
+      bar: "value"
+    }
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<property-editor id="id" type="type" prop-name="name" group="group"></property-editor>');
+    element = $compile(element)(scope);
+    scope.$digest();
+
+    expect(element.find('#foo').length).toBe(1);
+  }));
+});
