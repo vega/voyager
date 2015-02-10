06e549e198d00b4e937952eb5ff4d0f35982894e
Dominik Moritz <domoritz@gmail.com>
2015-02-09 23:33:57 -0800

    Also record dataset and config so that we can undo everything together

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('vleApp')
-  .controller('MainCtrl', function($scope, $document, Spec, consts, Chronicle, Logger) {
+  .controller('MainCtrl', function($scope, $document, Spec, Dataset, Config, consts, Chronicle, Logger) {
     $scope.Spec = Spec;
+    $scope.Dataset = Dataset;
+    $scope.Config = Config;
     $scope.consts = consts;
     $scope.showDevPanel = consts.debug;
 
@@ -10,36 +12,44 @@ angular.module('vleApp')
 
     $scope.canUndo = false;
     $scope.canRedo = false;
-    $scope.chron = Chronicle.record('spec', Spec, true);
 
-    $scope.canUndoRedo = function() {
-      $scope.canUndo = $scope.chron.canUndo();
-      $scope.canRedo = $scope.chron.canRedo();
-    };
-    $scope.chron.addOnAdjustFunction($scope.canUndoRedo);
-    $scope.chron.addOnUndoFunction($scope.canUndoRedo);
-    $scope.chron.addOnRedoFunction($scope.canUndoRedo);
+    // initialize undo after we have a dataset
+    Dataset.update(Dataset.dataset).then(function() {
+      Config.updateDataset(Dataset.dataset);
+      // Spec.update(Spec.spec);
 
-    $scope.chron.addOnUndoFunction(function() {
-      Logger.logInteraction('Undo');
-    });
-    $scope.chron.addOnRedoFunction(function() {
-      Logger.logInteraction('Redo');
-    });
+      $scope.chron = Chronicle.record('Spec.spec', $scope, true, ['Dataset.dataset', 'Dataset.dataschema', 'Dataset.stats', 'Config.config']);
 
-    angular.element($document).on('keydown', function(e) {
-      if (e.keyCode == 'Z'.charCodeAt(0) && (e.ctrlKey || e.metaKey) && !e.shiftKey) {
-        $scope.chron.undo();
-        $scope.$digest();
-        return false;
-      } else if (e.keyCode == 'Y'.charCodeAt(0) && (e.ctrlKey || e.metaKey)) {
-        $scope.chron.redo();
-        $scope.$digest();
-        return false;
-      } else if (e.keyCode == 'Z'.charCodeAt(0) && (e.ctrlKey || e.metaKey) && e.shiftKey) {
-        $scope.chron.redo();
-        $scope.$digest();
-        return false;
-      }
+      $scope.canUndoRedo = function() {
+        console.log('I registered a change');
+        $scope.canUndo = $scope.chron.canUndo();
+        $scope.canRedo = $scope.chron.canRedo();
+      };
+      $scope.chron.addOnAdjustFunction($scope.canUndoRedo);
+      $scope.chron.addOnUndoFunction($scope.canUndoRedo);
+      $scope.chron.addOnRedoFunction($scope.canUndoRedo);
+
+      $scope.chron.addOnUndoFunction(function() {
+        Logger.logInteraction('Undo');
+      });
+      $scope.chron.addOnRedoFunction(function() {
+        Logger.logInteraction('Redo');
+      });
+
+      angular.element($document).on('keydown', function(e) {
+        if (e.keyCode == 'Z'.charCodeAt(0) && (e.ctrlKey || e.metaKey) && !e.shiftKey) {
+          $scope.chron.undo();
+          $scope.$digest();
+          return false;
+        } else if (e.keyCode == 'Y'.charCodeAt(0) && (e.ctrlKey || e.metaKey)) {
+          $scope.chron.redo();
+          $scope.$digest();
+          return false;
+        } else if (e.keyCode == 'Z'.charCodeAt(0) && (e.ctrlKey || e.metaKey) && e.shiftKey) {
+          $scope.chron.redo();
+          $scope.$digest();
+          return false;
+        }
+      });
     });
   });
