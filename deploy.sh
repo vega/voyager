0d351052bee051061c75f4bba97efd70546db85f
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-02-10 23:02:08 -0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+gitsha=$(git rev-parse HEAD)
+
+git clone git@github.com:uwdata/vegalite-ui.git gh-pages
+cd gh-pages
+git checkout gh-pages
+cd ..
+gulp
+mv gh-pages/.git dist
+rm -rf gh-pages
+cd dist
+git add .
+git commit -am "release $gitsha"
+git push
+cd ..
