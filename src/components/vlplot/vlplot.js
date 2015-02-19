06b891abb98178ffb14dbce7450ff8903162c5c8
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-02-18 22:05:36 -0800

    update vlplot from facetedviz and also fixes #171 (vis bigger than max canvas size)

--- src/components/vlplot/vlplot.js
+++ src/components/vlplot/vlplot.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('vleApp')
-  .directive('vlPlot', function(vg, $timeout) {
+  .directive('vlPlot', function(vg, $timeout, $q, Dataset, consts) {
     var counter = 0;
-    var MAX_CANVAS_SIZE = 32767, MAX_CANVAS_AREA = 268435456;
+    var MAX_CANVAS_SIZE = 32767/2, MAX_CANVAS_AREA = 268435456/4;
 
-    function getRenderer(spec) {
+    function getRenderer(width, height) {
       // use canvas by default but use svg if the visualization is too big
-      if (spec.width > MAX_CANVAS_SIZE || spec.height > MAX_CANVAS_SIZE || spec.width*spec.height > MAX_CANVAS_AREA) {
+      if (width > MAX_CANVAS_SIZE || height > MAX_CANVAS_SIZE || width*height > MAX_CANVAS_AREA) {
         return 'svg';
       }
       return 'canvas';
@@ -18,8 +18,10 @@ angular.module('vleApp')
       restrict: 'E',
       scope: {
         'vgSpec':'=',
+        'encoding': '=',
         'maxHeight':'='
       },
+      replace: true,
       link: function(scope, element) {
         scope.visId = (counter++);
         scope.hoverAction = null;
@@ -34,6 +36,37 @@ angular.module('vleApp')
           scope.hoverFocus = scope.unlocked = false;
         };
 
+        function render() {
+          var start = new Date().getTime();
+          var spec= scope.vgSpec;
+          var shorthand = scope.encoding ? scope.encoding.toShorthand() : '';
+
+          scope.renderer = getRenderer(spec);
+
+          vg.parse.spec(spec, function(chart) {
+            var endParse = new Date().getTime();
+            view = null;
+            view = chart({el: element[0]});
+
+            if (!consts.useUrl) {
+              view.data({table: Dataset.data});
+            }
+
+            scope.width =  view.width();
+            scope.height = view.height();
+            view.renderer(getRenderer(spec.width, scope.height));
+            view.update();
+
+            var endChart = new Date().getTime();
+            console.log('parse spec', (endParse-start), 'charting', (endChart-endParse), shorthand);
+
+            view.on('mouseover', function(event, item) {
+              // TODO: Hanchuan please create tooltip from this
+              console.log(item.datum.data);
+            });
+          });
+        }
+
         var view;
         scope.$watch('vgSpec',function(spec) {
           if (!spec) {
@@ -42,17 +75,12 @@ angular.module('vleApp')
             }
             return;
           }
-          var elem = element;
-          if (elem) {
-            vg.parse.spec(spec, function(chart) {
-              view = null;
-              view = chart({el: elem[0], renderer: getRenderer(spec)});
-              view.update();
-              view.on('mouseover', function(event, item) {
-                // TODO: Hanchuan please create tooltip from this
-                console.log(item.datum.data);
-              });
-            });
+
+          scope.height = spec.height;
+
+          if (element) {
+            // $timeout(render, 10);
+            render();
           } else {
             console.error('can not find vis element');
           }
