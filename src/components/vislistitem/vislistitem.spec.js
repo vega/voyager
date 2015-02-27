8279562e27cade395dd0827f5cf49dcc7cd1d685
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-02-26 16:10:17 -0800

    fix tests

--- src/components/vislistitem/vislistitem.spec.js
+++ src/components/vislistitem/vislistitem.spec.js
@@ -8,13 +8,37 @@ describe('Directive: visListItem', function () {
   var element,
     scope;
 
+
+  beforeEach(module('vleApp', function($provide) {
+    // mock vega
+    $provide.constant('vg', {
+      parse: {
+        spec: function(spec, callback) {
+          callback(function(opt) {
+            element.append('<div></div>');
+            return {
+              width: function() {},
+              height: function() {},
+              update: function() {},
+              renderer: function() {},
+              on: function() {}
+            };
+          });
+        }
+      }
+    });
+    $provide.constant('vl', vl);
+  }));
+
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
+    scope.showExpand = true;
   }));
 
   it('should make hidden element visible', inject(function ($compile) {
     element = angular.element('<vis-list-item></vis-list-item>');
     element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the visListItem directive');
+    scope.$digest();
+    expect(element.find('.vis-list-item').length).toBe(1);
   }));
 });
