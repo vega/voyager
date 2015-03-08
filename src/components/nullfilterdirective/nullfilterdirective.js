9d7d183f9c91a6a99a2daccedbbdc057ad46c838
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-03-08 11:05:57 -0700

    update `nullfilterdirective` to update `Spec` instead of `Config`

--- src/components/nullfilterdirective/nullfilterdirective.js
+++ src/components/nullfilterdirective/nullfilterdirective.js
@@ -7,13 +7,13 @@
  * # nullFilterDirective
  */
 angular.module('vleApp')
-  .directive('nullFilterDirective', function (Spec, Config) {
+  .directive('nullFilterDirective', function (Spec) {
     return {
       templateUrl: 'components/nullfilterdirective/nullfilterdirective.html',
       restrict: 'E',
       scope: {},
       link: function postLink(scope, element, attrs) {
-        scope.Config = Config;
+        scope.Spec = Spec;
 
         scope.updateFilter = function() {
           Spec.update();
