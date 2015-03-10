b1c4aa6c656b06a4c6afa9fd2a2f8685b01edf17
Dominik Moritz <domoritz@gmail.com>
2015-03-10 10:49:18 -0700

    Add missing underscore -> space filter

---
+++ src/components/filters/underscore2space/underscore2space.filter.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Filter: underscore2space', function () {
+
+  // load the filter's module
+  beforeEach(module('vleApp'));
+
+  // initialize a new instance of the filter before each test
+  var underscore2space;
+  beforeEach(inject(function ($filter) {
+    underscore2space = $filter('underscore2space');
+  }));
+
+  it('should return the input with _ replaced', function () {
+    var text = 'foo_bar__baz';
+    expect(underscore2space(text)).toBe('foo bar baz');
+  });
+
+});
