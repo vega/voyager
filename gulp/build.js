61c0d303bd5b557cae770f8be79b0930487ab934
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-04-22 16:25:06 -0700

    update build.js using the new app name,

--- gulp/build.js
+++ gulp/build.js
@@ -19,7 +19,7 @@ gulp.task('partials', function () {
       quotes: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'vleApp'
+      module: 'polestar'
     }))
     .pipe(gulp.dest(paths.tmp + '/partials/'));
 });
