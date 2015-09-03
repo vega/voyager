9fa28f90ac100fdcfd3b8eca796a1021d2a37a92
Kanit Wongsuphasawat <kanitw@gmail.com>
2015-09-02 20:29:46 -0400

    Update propertyeditor.js

--- src/components/propertyeditor/propertyeditor.js
+++ src/components/propertyeditor/propertyeditor.js
@@ -25,6 +25,8 @@ angular.module('polestar')
       },
       link: function postLink(scope /*, element, attrs*/) {
         scope.hasAuto = scope.default === undefined;
+        
+        //TODO(kanitw): consider renaming
         scope.automodel = { value: false };
 
         if (scope.hasAuto) {
