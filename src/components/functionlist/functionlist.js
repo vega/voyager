cb743c23636fef255fd1ee1b0eabbab1e035ba11
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-11-18 19:09:23 -0800

    vl.Type => vl.type

--- src/components/functionlist/functionlist.js
+++ src/components/functionlist/functionlist.js
@@ -17,7 +17,7 @@ angular.module('voyager')
         };
 
         function getTimeUnits(type) {
-          return type === vl.Type.TEMPORAL ? vl.schema.timeUnits : [];
+          return type === vl.type.TEMPORAL ? vl.schema.timeUnits : [];
         }
 
         function getAggrs(type) {
@@ -54,11 +54,11 @@ angular.module('voyager')
             scope.func.list=[COUNT];
             scope.func.selected = COUNT;
           } else {
-            var isO = type === vl.Type.ORDINAL;
+            var isO = type === vl.type.ORDINAL;
             scope.func.list = ( isO ? [RAW] : [ANY, RAW])
               .concat(getTimeUnits(type))
               .concat(getAggrs(type).filter(function(x) { return x !== COUNT; }))
-              .concat(type === vl.Type.QUANTITATIVE ? [BIN] : []);
+              .concat(type === vl.type.QUANTITATIVE ? [BIN] : []);
 
             scope.func.selected = fieldDef._bin ? BIN :
               fieldDef._raw ? RAW :
