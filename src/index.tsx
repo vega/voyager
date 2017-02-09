0a53b58ce7e09d4b53d5f70ffafbff03e0bf8314
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-02-09 15:53:07 -0800

    Change Quotes

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import { Hello } from "./components/Hello";
+import { Hello } from './components/Hello';
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    <Hello compiler='TypeScript' framework='React' />,
+    document.getElementById('example')
 );
