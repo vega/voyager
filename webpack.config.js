751396001f4c24cd53f022108a485043662aefe2
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-02-12 11:08:26 -0800

    Use cheap-eval-source-map

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   },
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  devtool: 'cheap-eval-source-map',
+  // devtool: "source-map", // this is better for production
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
