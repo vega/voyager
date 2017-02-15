158a351410c554ac22599c4104093f4d7911ae84
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-02-15 11:44:16 -0800

    Add Function Selector Logic + Basic UI

--- src/models/shelf.test.ts
+++ src/models/shelf.test.ts
@@ -1,3 +1,4 @@
+import {SHORT_WILDCARD} from 'compassql/src/wildcard';
 import {toSpecQuery} from './shelf';
 
 describe('models/shelf', () => {
@@ -9,14 +10,14 @@ describe('models/shelf', () => {
           x: {field: 'a', type: 'quantitative'}
         },
         anyEncodings: [
-          {channel: '?', field: 'b', type: 'ordinal"'}
+          {channel: SHORT_WILDCARD, field: 'b', type: 'ordinal'}
         ],
         config: {numberFormat: 'd'}
       })).toEqual({
         mark: 'point',
         encodings: [
           {channel: 'x', field: 'a', type: 'quantitative'},
-          {channel: '?', field: 'b', type: 'ordinal"'}
+          {channel: '?', field: 'b', type: 'ordinal'}
         ],
         config: {numberFormat: 'd'}
       });
