2bd542ad80b39744da4e8a98c923b57564e8c32e
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-02-26 20:00:54 -0800

    Add Redux-thunk to support asynchronous actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
-import {createStore} from 'redux';
-import {} from 'redux-undo';
+import {createStore, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
 
 import {DEFAULT_STATE} from '../models';
 import {rootReducer} from '../reducers';
 
 export function configureStore(initialState = DEFAULT_STATE) {
-  const store = createStore(rootReducer, {past: [], present: initialState, future: []});
+  const store = createStore(
+    rootReducer,
+    {past: [], present: initialState, future: []},
+    applyMiddleware(thunkMiddleware)
+  );
   return store;
 }
