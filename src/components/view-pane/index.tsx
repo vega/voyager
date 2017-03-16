c03032ceab9375b4460f98b9be308bd9d8e395f5
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-03-15 22:08:29 -0700

    Distinguish between view-pane-specific and view-pane-gallery

--- src/components/view-pane/index.tsx
+++ src/components/view-pane/index.tsx
@@ -5,6 +5,7 @@ import {connect} from 'react-redux';
 import {FacetedUnitSpec} from 'vega-lite/build/src/spec';
 
 import {State} from '../../models';
+import {hasWildcards} from '../../models/shelf/spec';
 import {getMainSpec, getQuery} from '../../selectors';
 import {Plot} from '../plot';
 import * as styles from './view-pane.scss';
@@ -16,16 +17,28 @@ export interface ViewPaneProps {
 
 class ViewPaneBase extends React.PureComponent<ViewPaneProps, {}> {
   public render() {
-    return (
-      <div className="pane" styleName="view-pane">
-        <h2>Specified View</h2>
-        <Plot spec={this.props.mainSpec}/>
+    const {query} = this.props;
+    const isSpecific = !hasWildcards(query.spec).hasAnyWildcard;
 
-        {/*{JSON.stringify(this.props.query)}
+    if (isSpecific) {
+      return (
+        <div className="pane" styleName="view-pane-specific">
+          <h2>Specified View</h2>
+          <Plot spec={this.props.mainSpec}/>
 
-        {JSON.stringify(this.props.mainSpec)}*/}
-      </div>
-    );
+          {/*{JSON.stringify(this.props.query)}
+
+          {JSON.stringify(this.props.mainSpec)}*/}
+        </div>
+      );
+    } else {
+      return (
+        <div className="pane" styleName="view-pane-gallery">
+          <h2>Specified Views</h2>
+        </div>
+      );
+    }
+
   }
 }
 export const ViewPane = connect(
