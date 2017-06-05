vega/voyager

commit f8ac89ee88a0f195f747aebd36dfb309c600c76d

325 data source config (#340)
* Add config reducer and action creator

* Allow hiding of the dataset selector via a config option.

* Move VoyagerConfig definition into its own model file.

* fix lint errors

* remove setConfig action creator

src/components/app.tsx
@@ -8,16 +8,16 @@ import * as SplitPane from 'react-split-pane';
 import {Dispatch} from 'redux';
 import {StateWithHistory} from 'redux-undo';
 import {Data, InlineData, isInlineData, isUrlData, UrlData} from 'vega-lite/build/src/data';
-import {datasetReceive, datasetUrlLoad} from '../actions';
+import { datasetReceive, datasetUrlLoad, SET_CONFIG } from '../actions';
 import {StateBase} from '../models/index';
 
+import {VoyagerConfig} from '../models/config';
 import {DataPane} from './data-pane';
 import {EncodingPane} from './encoding-pane';
 import {Header} from './header';
 import {ViewPane} from './view-pane';
 
 
-export type VoyagerConfig = Object;
 export type VoyagerData = Data;
 
 interface Props extends React.Props<AppBase> {
@@ -56,7 +56,7 @@ class AppBase extends React.PureComponent<Props, {}> {
   }
 
   private update(props: Props) {
-    const { data } = props;
+    const { data, config } = props;
     if (data) {
       if (isUrlData(data)) {
         this.loadDataFromUrl(data);
@@ -64,6 +64,8 @@ class AppBase extends React.PureComponent<Props, {}> {
         this.loadData(data);
       }
     }
+
+    this.setConfig(config);
   }
 
   private loadData(data: InlineData) {
@@ -74,6 +76,15 @@ class AppBase extends React.PureComponent<Props, {}> {
     if (this.props.dispatch) {
       this.props.dispatch(datasetUrlLoad("Custom Data", data.url));
     }
+  }
+
+  private setConfig(config: VoyagerConfig) {
+    this.props.dispatch({
+      type: SET_CONFIG,
+      payload: {
+        config,
+      }
+    });
   }
 }
 

src/components/data-pane/index.tsx
@@ -6,22 +6,25 @@ import * as styles from './data-pane.scss';
 
 import {ActionHandler, createDispatchHandler, DatasetAsyncAction, ShelfFieldAutoAdd} from '../../actions';
 import {Dataset, State} from '../../models';
+import {VoyagerConfig} from '../../models/config';
 import {DatasetSelector} from './dataset-selector';
 import {FieldList, PresetWildcardFieldList} from './field-list';
 
 export interface DataPanelProps extends ActionHandler<DatasetAsyncAction | ShelfFieldAutoAdd> {
   data: Dataset;
+  config: VoyagerConfig;
 }
 
 export class DataPanelBase extends React.PureComponent<DataPanelProps, {}> {
   public render() {
     const {handleAction} = this.props;
     const {name} = this.props.data;
+    const {showDataSourceSelector} = this.props.config;
 
     return (
       <div className="pane" styleName="data-pane">
         <h2>Data</h2>
-        <DatasetSelector name={name} handleAction={handleAction}/>
+        {showDataSourceSelector ? <DatasetSelector name={name} handleAction={handleAction}/> : undefined}
         <div>Name: {name}</div>
 
         <div styleName="data-pane-section">
@@ -40,7 +43,10 @@ export class DataPanelBase extends React.PureComponent<DataPanelProps, {}> {
 
 export const DataPane = connect(
   (state: State) => {
-    return {data: state.present.dataset};
+    return {
+      data: state.present.dataset,
+      config: state.present.config
+    };
   },
   createDispatchHandler<DatasetAsyncAction>()
 )(CSSModules(DataPanelBase, styles));

src/actions/config.ts
@@ -1,0 +1,9 @@
+import {VoyagerConfig} from '../models/config';
+import {ReduxAction} from './redux-action';
+
+export type ConfigAction = SetConfig;
+
+export const SET_CONFIG = 'SET_CONFIG';
+export type SetConfig = ReduxAction<typeof SET_CONFIG, {
+  config: VoyagerConfig
+}>;

src/actions/index.ts
@@ -1,9 +1,11 @@
-
+import {ConfigAction} from './config';
 import {DatasetAction} from './dataset';
 import {ResultAction} from './result';
 import {ShelfAction} from './shelf';
 import {UndoableAction} from './undo-redo';
 
+
+export * from './config';
 export * from './dataset';
 export * from './result';
 export * from './redux-action';
@@ -13,4 +15,4 @@ export * from './undo-redo';
 /**
  * Union type of all actions in our application.
  */
-export type Action = DatasetAction | ShelfAction | UndoableAction | ResultAction;
+export type Action = DatasetAction | ShelfAction | UndoableAction | ResultAction | ConfigAction;

src/lib-voyager.tsx
@@ -13,7 +13,8 @@ import {Store} from 'redux';
 import 'font-awesome-sass-loader'; // TODO should this move to App?
 import {StateWithHistory} from 'redux-undo';
 import {isString} from 'vega-lite/build/src/util';
-import {App, VoyagerConfig, VoyagerData} from './components/app';
+import {App, VoyagerData} from './components/app';
+import {VoyagerConfig} from './models/config';
 import {StateBase} from './models/index';
 import {configureStore} from './store';
 

src/models/config.ts
@@ -1,0 +1,7 @@
+export interface VoyagerConfig {
+  showDataSourceSelector?: boolean;
+};
+
+export const DEFAULT_VOYAGER_CONFIG = {
+  showDataSourceSelector: false
+};

src/models/index.ts
@@ -1,5 +1,6 @@
 import {StateWithHistory} from 'redux-undo';
 
+import {DEFAULT_VOYAGER_CONFIG, VoyagerConfig} from './config';
 import {Dataset, DEFAULT_DATASET} from './dataset';
 import {DEFAULT_RESULT, ResultIndex} from './result';
 import {DEFAULT_SHELF_SPEC, Shelf} from './shelf';
@@ -12,6 +13,7 @@ export * from './result';
  * Application state.
  */
 export interface StateBase {
+  config: VoyagerConfig;
   dataset: Dataset;
   shelf: Shelf;
   result: ResultIndex;
@@ -23,6 +25,7 @@ export interface StateBase {
 export type State = StateWithHistory<StateBase>;
 
 export const DEFAULT_STATE: StateBase = {
+  config: DEFAULT_VOYAGER_CONFIG,
   dataset: DEFAULT_DATASET,
   shelf: DEFAULT_SHELF_SPEC,
   result: DEFAULT_RESULT,

src/reducers/config.test.ts
@@ -1,0 +1,38 @@
+import { SET_CONFIG } from '../actions/config';
+import { configReducer } from './config';
+
+describe('reducers/config', () => {
+  describe(SET_CONFIG, () => {
+    it('returns new voyager state with config.showDatasetSelector set to true', () => {
+      expect(configReducer(
+        {},
+        {
+          type: SET_CONFIG,
+          payload: {
+            config: {
+              showDatasetSelector: true
+            }
+          }
+        }
+      )).toEqual({
+        showDatasetSelector: true
+      });
+    });
+
+    it('returns new voyager state with config.showDatasetSelector set to false', () => {
+      expect(configReducer(
+        {},
+        {
+          type: SET_CONFIG,
+          payload: {
+            config: {
+              showDatasetSelector: false
+            }
+          }
+        }
+      )).toEqual({
+        showDatasetSelector: false
+      });
+    });
+  });
+});

commit e424e5816c0af6e8c26bd2c454e5d600d23ea96b

fixes a regression introduced in #340. show dataset selector in app

src/components/app.tsx
@@ -65,7 +65,9 @@ class AppBase extends React.PureComponent<Props, {}> {
       }
     }
 
-    this.setConfig(config);
+    if (config) {
+      this.setConfig(config);
+    }
   }
 
   private loadData(data: InlineData) {

src/index.tsx
@@ -6,10 +6,13 @@ import 'font-awesome-sass-loader';
 
 import { Data } from 'vega-lite/build/src/data';
 import { App } from './components/app';
+import { VoyagerConfig } from './models/config';
 import { configureStore } from './store';
 
 const store = configureStore();
-const config = {};
+const config: VoyagerConfig = {
+  showDataSourceSelector: true
+};
 const data: Data = undefined;
 
 ReactDOM.render(