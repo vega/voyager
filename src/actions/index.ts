0343257076b25c177fa8f11393ac68da0b3a1d95
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-06-15 10:23:20 -0700

    Create an ACTION_TYPES array that contains all action types (#381)

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -16,3 +16,34 @@ export * from './undo-redo';
  * Union type of all actions in our application.
  */
 export type Action = DatasetAction | ShelfAction | UndoableAction | ResultAction | ConfigAction;
+
+export type ActionType = Action['type'];
+
+// Use type to enforce that ACTION_TYPE_INDEX contains all action types.
+const ACTION_TYPE_INDEX: {[k in ActionType]: 1} = {
+  DATASET_URL_REQUEST: 1,
+  DATASET_URL_RECEIVE: 1,
+  DATASET_INLINE_RECEIVE: 1,
+
+  RESULT_RECEIVE: 1,
+  RESULT_REQUEST: 1,
+
+  SET_CONFIG: 1,
+
+  SHELF_CLEAR: 1,
+  SHELF_MARK_CHANGE_TYPE: 1,
+  SHELF_FIELD_ADD: 1,
+  SHELF_FIELD_AUTO_ADD: 1,
+  SHELF_FIELD_MOVE: 1,
+  SHELF_FIELD_REMOVE: 1,
+  SHELF_FUNCTION_CHANGE: 1,
+  SHELF_SPEC_LOAD: 1,
+  SHELF_SPEC_PREVIEW: 1,
+  SHELF_SPEC_PREVIEW_DISABLE: 1,
+
+  UNDO: 1,
+  REDO: 1,
+};
+
+/** An array of all possible action types. */
+export const ACTION_TYPES = Object.keys(ACTION_TYPE_INDEX) as ActionType[];
