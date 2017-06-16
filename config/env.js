58987200c5ae08fe5045ed02a0f35b3ff151a66f
Jim Vallandingham <vlandham@gmail.com>
2017-06-16 10:14:11 -0700

    Add SERVER env variable for setting serverUrl.

--- config/env.js
+++ config/env.js
@@ -23,12 +23,14 @@ function getClientEnvironment(publicUrl) {
       {
         // Useful for determining whether we’re running in production mode.
         // Most importantly, it switches React into the correct mode.
-        NODE_ENV: process.env.NODE_ENV, 
+        NODE_ENV: process.env.NODE_ENV,
         // Useful for resolving the correct path to static assets in `public`.
         // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
         // This should only be used as an escape hatch. Normally you would put
         // images into the `src` and `import` them in code to get their paths.
         PUBLIC_URL: publicUrl,
+        // run server-mode or local
+        SERVER: process.env.SERVER
       }
     );
   // Stringify all values so we can feed into Webpack DefinePlugin
