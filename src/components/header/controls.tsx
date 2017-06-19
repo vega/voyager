82fd6a33fdb7761bdbc044dc572f05d655cd3d62
starry <starrynight0225@gmail.com>
2017-06-19 11:58:16 -0700

    implemented header ui

---
+++ src/components/header/controls.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import * as CSSModules from 'react-css-modules';
+import { Bookmark } from './bookmark';
+import * as styles from './controls.scss';
+import { UndoRedo } from './undo-redo';
+
+class ControlsBase extends React.Component<any, any> {
+  public render() {
+    return (
+      <div styleName='controls'>
+        <Bookmark/>
+        <UndoRedo/>
+      </div>
+    );
+  }
+}
+
+export const Controls = CSSModules(ControlsBase, styles);
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
-import {UndoRedo} from './undo-redo';
+import * as CSSModules from 'react-css-modules';
+import {Controls} from './controls';
+import * as styles from './header.scss';
 
-export class Header extends React.PureComponent<any, {}> {
+export class HeaderBase extends React.PureComponent<any, {}> {
   public render() {
     return (
-      <header className="header">
-        Voyager 2
-        <UndoRedo/>
-      </header>
+      <div styleName='header'>
+        <img styleName='voyager-logo' src={'../../../images/logo.png'}/>
+        <Controls />
+        <a styleName='idl-logo' href='https://idl.cs.washington.edu/'>
+          <img src={'../../../images/idl-h56.png'}/>
+        </a>
+      </div>
     );
   }
 }
+
+export const Header = CSSModules(HeaderBase, styles);
