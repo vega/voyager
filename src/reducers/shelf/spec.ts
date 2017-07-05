9aa622c417fee1f93ad70e6112773bdd10b27cf0
Felix Ouk <felix.ouk@gmail.com>
2017-07-05 14:03:01 -0700

    Move array util to `reducers/util.ts`

--- src/reducers/shelf/spec.ts
+++ src/reducers/shelf/spec.ts
@@ -4,6 +4,7 @@ import {recommend} from 'compassql/build/src/recommend';
 import {Schema} from 'compassql/build/src/schema';
 import {isWildcard, SHORT_WILDCARD} from 'compassql/build/src/wildcard';
 
+import {insertItemToArray, modifyItemInArray, removeItemFromArray} from '../util';
 
 import {Action} from '../../actions';
 import {
@@ -210,33 +211,4 @@ function removeEncoding(shelf: Readonly<ShelfUnitSpec>, shelfId: ShelfId):
       }
     };
   }
-}
-
-/**
- * Immutable array splice
- */
-function removeItemFromArray(array: ReadonlyArray<any>, index: number) {
-  return {
-    item: array[index],
-    array: [
-      ...array.slice(0, index),
-      ...array.slice(index + 1)
-    ]
-  };
-}
-
-function insertItemToArray<T>(array: ReadonlyArray<T>, index: number, item: T) {
-  return [
-    ...array.slice(0, index),
-    item,
-    ...array.slice(index)
-  ];
-}
-
-function modifyItemInArray<T>(array: ReadonlyArray<T>, index: number, modifier: (t: Readonly<T>) => T) {
-  return [
-    ...array.slice(0, index),
-    modifier(array[index]),
-    ...array.slice(index + 1)
-  ];
 }
