710b3cebcf38a7ae0f586c221164f2f888ef14ce
starry <starrynight0225@gmail.com>
2017-07-17 14:52:23 -0700

    rewrite field to have popup; click outside to close popup

--- src/components/data-pane/field-list.tsx
+++ src/components/data-pane/field-list.tsx
@@ -1,11 +1,8 @@
+import {ExpandedType} from 'compassql/build/src/query/expandedtype';
+import {PrimitiveType, Schema} from 'compassql/build/src/schema';
 import * as React from 'react';
 import * as CSSModules from 'react-css-modules';
 import {connect} from 'react-redux';
-import * as TetherComponent from 'react-tether';
-import * as styles from './field-list.scss';
-
-import {ExpandedType} from 'compassql/build/src/query/expandedtype';
-import {PrimitiveType, Schema} from 'compassql/build/src/schema';
 import {DatasetSchemaChangeFieldType} from '../../actions/dataset';
 import {ActionHandler, createDispatchHandler} from '../../actions/redux-action';
 import {SHELF_FIELD_AUTO_ADD, ShelfFieldAutoAdd} from '../../actions/shelf';
@@ -14,6 +11,7 @@ import {State} from '../../models/index';
 import {ShelfFieldDef} from '../../models/shelf/encoding';
 import {getPresetWildcardFields, getSchema, getSchemaFieldDefs} from '../../selectors';
 import {Field} from '../field';
+import * as styles from './field-list.scss';
 import {TypeChanger} from './type-changer';
 
 
@@ -22,20 +20,13 @@ export interface FieldListProps extends ActionHandler<ShelfFieldAutoAdd | Datase
   schema: Schema;
 }
 
-export interface FieldListState {
-  selectedField: string;
-}
-
-class FieldListBase extends React.PureComponent<FieldListProps, FieldListState> {
+class FieldListBase extends React.PureComponent<FieldListProps, {}> {
 
   constructor(props: FieldListProps) {
     super(props);
 
     // Bind - https://facebook.github.io/react/docs/handling-events.html
     this.onAdd = this.onAdd.bind(this);
-    this.state = {
-      selectedField: null
-    };
   }
 
   public render() {
@@ -69,23 +60,16 @@ class FieldListBase extends React.PureComponent<FieldListProps, FieldListState> 
 
   private renderComponent(fieldDef: ShelfFieldDef, hideTypeChanger: boolean, primitiveType: PrimitiveType) {
     if (hideTypeChanger) {
-      return this.renderField(fieldDef, hideTypeChanger);
+      return this.renderField(fieldDef);
     } else {
-      return (
-        <TetherComponent
-          attachment="top left"
-          targetAttachment="bottom left"
-        >
-        {this.renderField(fieldDef, hideTypeChanger)}
-        {this.renderTypeChanger(fieldDef, primitiveType)}
-        </TetherComponent>
-      );
+      const popupComponent = this.renderTypeChanger(fieldDef, primitiveType);
+      return this.renderField(fieldDef, popupComponent);
     }
   }
 
   private renderTypeChanger(fieldDef: ShelfFieldDef, primitiveType: PrimitiveType) {
     const {handleAction} = this.props;
-    if (typeof fieldDef.field === 'string' && this.state.selectedField === fieldDef.field) {
+    if (typeof fieldDef.field === 'string') {
       return (
         <TypeChanger
           field={fieldDef.field}
@@ -97,31 +81,18 @@ class FieldListBase extends React.PureComponent<FieldListProps, FieldListState> 
     }
   }
 
-  private renderField(fieldDef: ShelfFieldDef, hideTypeChanger: boolean) {
+  private renderField(fieldDef: ShelfFieldDef, popupComponent?: JSX.Element) {
     return (
       <Field
         fieldDef={fieldDef}
         isPill={true}
         draggable={true}
         parentId={{type: FieldParentType.FIELD_LIST}}
-        caretHide={hideTypeChanger}
-        caretOnClick={this.handleCaretClick.bind(this, fieldDef.field)}
         onDoubleClick={this.onAdd}
         onAdd={this.onAdd}
+        popupComponent={popupComponent}
       />
     );
-  }
-
-  private handleCaretClick(field: string) {
-    if (this.state.selectedField === field) {
-      this.setState({
-        selectedField: null
-      });
-    } else {
-      this.setState({
-        selectedField: field
-      });
-    }
   }
 
   private getValidTypes(primitiveType: PrimitiveType): ExpandedType[] {
