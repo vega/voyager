998026dd2dc52483d62761ab06190bcfe3c2e6c5
Yannick Assogba <yassogba@gmail.com>
2017-07-20 17:41:38 -0400

    Load header images with web pack loader.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,14 +3,17 @@ import * as CSSModules from 'react-css-modules';
 import {Controls} from './controls';
 import * as styles from './header.scss';
 
+import * as idlLogo from '../../../images/idl-h56.png';
+import * as logo from '../../../images/logo.png';
+
 export class HeaderBase extends React.PureComponent<{}, {}> {
   public render() {
     return (
       <div styleName='header'>
-        <img styleName='voyager-logo' src={'../../../images/logo.png'}/>
+        <img styleName='voyager-logo' src={logo}/>
         <Controls />
         <a styleName='idl-logo' href='https://idl.cs.washington.edu/'>
-          <img src={'../../../images/idl-h56.png'}/>
+          <img src={idlLogo}/>
         </a>
       </div>
     );
