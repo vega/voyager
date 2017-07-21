1b5c651a35a2418e3f562d076cca87625a91c2c4
starry <starrynight0225@gmail.com>
2017-07-21 11:59:01 -0700

    Rewrite mainResult selector to avoid re-creating spec if not necessary

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -1,28 +1,43 @@
 import {Schema} from 'compassql/build/src/schema';
 import {SHORT_WILDCARD} from 'compassql/build/src/wildcard';
 import {createSelector} from 'reselect';
-
 import {Shelf, ShelfFieldDef, State, toQuery} from '../models';
-
 
 // Imports to satisfy --declarations build requirements
 // https://github.com/Microsoft/TypeScript/issues/9944
 // tslint:disable-next-line:no-unused-variable
-import {SpecQueryGroup} from 'compassql/build/src/model';
+import {getTopSpecQueryItem, SpecQueryGroup} from 'compassql/build/src/model';
 // tslint:disable-next-line:no-unused-variable
 import {Query} from 'compassql/build/src/query/query';
 // tslint:disable-next-line:no-unused-variable
 import {StateWithHistory} from 'redux-undo';
 // tslint:disable-next-line:no-unused-variable
-import {InlineData, NamedData, UrlData} from 'vega-lite/build/src/data';
+import {Selector} from 'reselect/src/reselect';
+// tslint:disable-next-line:no-unused-variable
+import {BoxPlotDef} from 'vega-lite/build/src/compositemark/boxplot';
+// tslint:disable-next-line:no-unused-variable
+import {Data, InlineData, NamedData, UrlData} from 'vega-lite/build/src/data';
+// tslint:disable-next-line:no-unused-variable
+import {EncodingWithFacet} from 'vega-lite/build/src/encoding';
 // tslint:disable-next-line:no-unused-variable
 import {OneOfFilter, RangeFilter} from 'vega-lite/build/src/filter';
 // tslint:disable-next-line:no-unused-variable
-import {StateBase, VoyagerConfig} from '../models';
+import {MarkDef} from 'vega-lite/build/src/mark';
+// tslint:disable-next-line:no-unused-variable
+import {FacetedCompositeUnitSpec, GenericUnitSpec} from 'vega-lite/build/src/spec';
 // tslint:disable-next-line:no-unused-variable
 import {Bookmark} from '../models/bookmark';
 // tslint:disable-next-line:no-unused-variable
+import {VoyagerConfig} from '../models/config';
+// tslint:disable-next-line:no-unused-variable
+import {StateBase} from '../models/index';
+// tslint:disable-next-line:no-unused-variable
 import {PlotObject} from '../models/plot';
+import {extractPlotObjects} from '../models/plot';
+import {Result} from '../models/result';
+import {getTransforms, hasWildcards} from '../models/shelf/spec';
+
+
 
 export const selectBookmark = (state: State) => state.present.bookmark;
 export const selectConfig = (state: State) => state.present.config;
@@ -30,7 +45,6 @@ export const selectData = (state: State) => state.present.dataset.data;
 export const selectFilters = (state: State) => state.present.shelf.spec.filters;
 export const selectShelf = (state: State) => state.present.shelf;
 export const selectSchema = (state: State) => state.present.dataset.schema;
-export const selectMainResult = (state: State) => state.present.result.main.modelGroup;
 
 export const selectQuery = createSelector(
   selectShelf,
@@ -65,5 +79,44 @@ export const selectSchemaFieldDefs = createSelector(
       const {name, vlType} = fieldSchema;
       return {field: name, type: vlType};
     });
+  }
+);
+
+const selectMainResult = (state: State) => state.present.result.main;
+
+const selectMainSpec = createSelector(
+  selectData, selectFilters, selectMainResult,
+  (data: Data, filters: Array<RangeFilter|OneOfFilter>, result: Result): FacetedCompositeUnitSpec => {
+    if (!result.modelGroup) {
+      return;
+    }
+    return {
+      data: data,
+      transform: getTransforms(filters),
+      ...getTopSpecQueryItem(result.modelGroup).spec
+    };
+  }
+);
+
+export const selectMainResultForView = createSelector(
+  selectQuery, selectMainResult, selectMainSpec,
+  (query: Query, result: Result, spec: FacetedCompositeUnitSpec): {
+    spec: FacetedCompositeUnitSpec,
+    plots: PlotObject[]
+  } => {
+    if (result.isLoading || !result.modelGroup) {
+      return;
+    }
+    if (!hasWildcards(query.spec).hasAnyWildcard) {
+      return {
+        spec,
+        plots: null
+      };
+    } else {
+      return {
+        spec: null,
+        plots: extractPlotObjects(result.modelGroup)
+      };
+    }
   }
 );
