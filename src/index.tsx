f1aeb7e8b380837459d2b92249e1b138f438c8de
Yannick Assogba <yassogba@gmail.com>
2017-07-26 12:22:23 -0400

    code to demonstrate bug

--- src/index.tsx
+++ src/index.tsx
@@ -13,15 +13,22 @@ import { configureStore } from './store';
 const store = configureStore();
 const config: VoyagerConfig = VOYAGER_CONFIG;
 
-const data: Data = undefined;
+const data: Data = {
+  values: [
+    {date: "24-Apr-07", close: "93.24"},
+    {date: "25-Apr-07", close: "95.35"},
+    {date: "26-Apr-07", close: "98.84"},
+    {date: "27-Apr-07", close: "99.92"},
+  ]
+};
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App
-            config={config}
-            data={data}
-            dispatch={store.dispatch}
-        />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App
+      config={config}
+      data={data}
+      dispatch={store.dispatch}
+    />
+  </Provider>,
+  document.getElementById('root')
 );
