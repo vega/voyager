5b47e35de890951fbab10639c9036610d8c272c4
starry <starrynight0225@gmail.com>
2017-07-31 16:01:57 -0700

    remove TODO

--- src/reducers/shelf/index.ts
+++ src/reducers/shelf/index.ts
@@ -11,7 +11,6 @@ export function shelfReducer(shelf: Readonly<Shelf> = DEFAULT_SHELF_SPEC, action
 
   if (spec !== shelf.spec) {
     // Make sure we only re-create a new object if something has changed.
-    // TODO: decouple specPreview from shelf as it does not affect the compiled query.
     // TODO: once we have more query-based property here, better use some combineReducers() like function.
     // The problem is that combineReducer does not support additional parameter like `schema`
     // that we need for `shelfSpecReducer`
