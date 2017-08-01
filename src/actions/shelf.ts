de61788cb1fb8259d5a3ac441b5bfe401072f3a9
starry <starrynight0225@gmail.com>
2017-08-01 11:02:50 -0700

    remove unused action

--- src/actions/shelf.ts
+++ src/actions/shelf.ts
@@ -57,10 +57,3 @@ export type ShelfSpecLoad = ReduxAction<typeof SHELF_SPEC_LOAD, {
   spec: FacetedCompositeUnitSpec
 }>;
 
-export const SHELF_SPEC_PREVIEW = 'SHELF_SPEC_PREVIEW';
-export type ShelfSpecPreview = ReduxAction<typeof SHELF_SPEC_PREVIEW, {
-  spec: FacetedCompositeUnitSpec
-}>;
-
-export const SHELF_SPEC_PREVIEW_DISABLE = 'SHELF_SPEC_PREVIEW_DISABLE';
-export type ShelfSpecPreviewDisable = PlainReduxAction<typeof SHELF_SPEC_PREVIEW_DISABLE>;
