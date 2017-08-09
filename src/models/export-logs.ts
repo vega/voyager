789709a07ac06acf083bb0c9afe6d7b081205ce8
starry97 <starrynight0225@gmail.com>
2017-08-09 16:31:43 -0700

    Add IOSString in redux action logs

--- src/models/export-logs.ts
+++ src/models/export-logs.ts
@@ -1,10 +1,11 @@
 
 export function constructLogString(inputLogs: Array<{action: {type: string, payload: any}, timestamp: number}>):
-  Array<{type: string, timestamp: number, payload: string}> {
+  Array<{type: string, timestamp: number, ISOString: string, payload: string}> {
   const outputLogs = [];
   for (const inputLog of inputLogs) {
     outputLogs.push({
       timestamp: inputLog.timestamp,
+      ISOString: new Date(inputLog.timestamp).toISOString(),
       type: inputLog.action.type,
       payload: JSON.stringify(inputLog.action.payload)
     });
