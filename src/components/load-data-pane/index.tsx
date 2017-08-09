64b7c4bf5c362dfa49e726bd88ca5dc1f8d69b9d
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-08-09 15:55:47 -0700

    Don't show <DataSelector> in electron

--- src/components/load-data-pane/index.tsx
+++ src/components/load-data-pane/index.tsx
@@ -1,18 +1,43 @@
 import * as React from 'react';
 import * as CSSModules from 'react-css-modules';
+import {connect} from 'react-redux';
+import {VoyagerConfig} from '../../models/config';
+import {State} from '../../models/index';
+import {selectConfig} from '../../selectors/index';
 import {DataSelector} from '../data-selector';
 import * as styles from './load-data-pane.scss';
 
-export class LoadDataBase extends React.PureComponent<{}, {}> {
+
+export interface DataPanelProps {
+  config: VoyagerConfig;
+}
+
+export class LoadDataBase extends React.PureComponent<DataPanelProps, {}> {
   public render() {
-    return (
-      <div className="pane" styleName="load-data-pane">
-        Please load a dataset
-        {' '}
-        <DataSelector title="Load"/>
-      </div>
-    );
+    const {showDataSourceSelector} = this.props.config;
+    if (showDataSourceSelector) {
+      return (
+        <div className="pane" styleName="load-data-pane">
+          Please load a dataset
+          {' '}
+          <DataSelector title="Load"/>
+        </div>
+      );
+    } else {
+      // TODO: Make this a config parameter of lib-voyager
+      return (
+        <div className="pane" styleName="load-data-pane">
+          Please load a dataset.  (For the Electron app, please use the menu bar.)
+        </div>
+      );
+    }
   }
 }
 
-export const LoadData = CSSModules(LoadDataBase, styles);
+export const LoadData = connect(
+  (state: State) => {
+    return {
+      config: selectConfig(state)
+    };
+  }
+)(CSSModules(LoadDataBase, styles));
