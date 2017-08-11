99437ca1d5d35abd3ad7e5084d3ee4f9feb5e459
Felix Ouk <felix.ouk@gmail.com>
2017-08-11 16:53:17 -0700

    Move header/bookmark.tsx to bookmark/index.tsx

--- src/components/header/controls.tsx
+++ src/components/header/controls.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as CSSModules from 'react-css-modules';
-import {BookmarkPane} from './bookmark';
+import {BookmarkPane} from '../bookmark';
 import * as styles from './controls.scss';
 import { UndoRedo } from './undo-redo';
 
