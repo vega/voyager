52b337cb7fa02a6a20d2e9a82bb6b8c2c5ebc571
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-08-17 15:28:19 -0700

    Add ShelfPreviewQuery for Related View's specify button too.
    Also rename ShelfPreviewSpecDisable to ShelfPreviewDisable
    since it's useful for both PreviewSpec and PreviewQuery.

--- src/actions/shelf-preview.ts
+++ src/actions/shelf-preview.ts
@@ -1,12 +1,18 @@
+import {Query} from 'compassql/build/src/query/query';
 import {FacetedCompositeUnitSpec} from 'vega-lite/build/src/spec';
 import {PlainReduxAction, ReduxAction} from './redux-action';
 
-export type ShelfPreviewAction = ShelfPreviewSpec | ShelfPreviewSpecDisable;
+export type ShelfPreviewAction = ShelfPreviewSpec | ShelfPreviewQuery | ShelfPreviewDisable;
 
 export const SHELF_PREVIEW_SPEC = 'SHELF_PREVIEW_SPEC';
 export type ShelfPreviewSpec = ReduxAction<typeof SHELF_PREVIEW_SPEC, {
   spec: FacetedCompositeUnitSpec
 }>;
 
-export const SHELF_PREVIEW_SPEC_DISABLE = 'SHELF_PREVIEW_SPEC_DISABLE';
-export type ShelfPreviewSpecDisable = PlainReduxAction<typeof SHELF_PREVIEW_SPEC_DISABLE>;
+export const SHELF_PREVIEW_QUERY = 'SHELF_PREVIEW_QUERY';
+export type ShelfPreviewQuery = ReduxAction<typeof SHELF_PREVIEW_QUERY, {
+  query: Query
+}>;
+
+export const SHELF_PREVIEW_DISABLE = 'SHELF_PREVIEW_DISABLE';
+export type ShelfPreviewDisable = PlainReduxAction<typeof SHELF_PREVIEW_DISABLE>;
