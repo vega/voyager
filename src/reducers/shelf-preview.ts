3258dd178c19ca71ff095311ad63929afe423e6d
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-08-17 19:16:56 -0700

    Clean shelf preview when loading a spec/query

--- src/reducers/shelf-preview.ts
+++ src/reducers/shelf-preview.ts
@@ -1,6 +1,8 @@
 import {fromSpec} from 'compassql/build/src/query/spec';
 import {Action} from '../actions/index';
 import {SHELF_PREVIEW_DISABLE, SHELF_PREVIEW_QUERY, SHELF_PREVIEW_SPEC} from '../actions/shelf-preview';
+import {SHELF_LOAD_QUERY} from '../actions/shelf/index';
+import {SPEC_LOAD} from '../actions/shelf/spec';
 import {DEFAULT_SHELF_PREVIEW, ShelfPreview} from '../models/shelf-preview';
 import {fromSpecQuery} from '../models/shelf/spec';
 
@@ -19,6 +21,10 @@ export function shelfPreviewReducer(preview: ShelfPreview = DEFAULT_SHELF_PREVIE
       return {
         spec: fromSpecQuery(specQ)
       };
+
+    // Spec Loading should also clear shelf preview
+    case SPEC_LOAD:
+    case SHELF_LOAD_QUERY:
     case SHELF_PREVIEW_DISABLE:
       return {spec: null};
   }
