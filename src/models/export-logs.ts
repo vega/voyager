eeec49dd3e53e0197597cc39f0b5c2eed33ea4ec
starry <starrynight0225@gmail.com>
2017-08-30 16:17:03 -0700

    Do not output inline data since it makes the log file too big

--- src/models/export-logs.ts
+++ src/models/export-logs.ts
@@ -1,13 +1,25 @@
+import {isInlineData} from 'vega-lite/build/src/data';
+import {DATASET_RECEIVE} from '../actions/dataset';
+
 
 export function constructLogString(inputLogs: Array<{action: {type: string, payload: any}, timestamp: number}>):
   Array<{type: string, timestamp: number, ISOString: string, payload: string}> {
   const outputLogs = [];
   for (const inputLog of inputLogs) {
+    const type = inputLog.action.type;
+    let payload: any = inputLog.action.payload;
+    if (type === DATASET_RECEIVE && isInlineData(payload.data)) {
+      // don't output inline data because it might make the log file too big
+      payload = {
+        ...payload,
+        data: {name: 'source'}
+      };
+    }
     outputLogs.push({
       timestamp: inputLog.timestamp,
       ISOString: new Date(inputLog.timestamp).toISOString(),
-      type: inputLog.action.type,
-      payload: JSON.stringify(inputLog.action.payload)
+      type,
+      payload: JSON.stringify(payload)
     });
   }
   return outputLogs;
