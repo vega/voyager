bea454b6ff4799aacc636aa9f3d4bd879698f069
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-09-01 14:37:58 -0700

    Apply filter in `resultRequest` for now.  (We will remove this once we use the data flow API.)

--- src/store/listener.ts
+++ src/store/listener.ts
@@ -2,12 +2,18 @@ import {Query} from 'compassql/build/src/query/query';
 import {Store} from 'redux';
 import {Data} from 'vega-lite/build/src/data';
 import {State} from '../models/index';
+import {ShelfFilter} from '../models/shelf/filter';
 import {dispatchQueries} from '../queries/index';
 import {selectData, selectQuery} from '../selectors/index';
+import {selectFilters} from '../selectors/shelf';
 
 export function createQueryListener(store: Store<State>) {
-  let data: Data;
   let query: Query;
+
+  // TODO: remove data and filter once we use dataflow api to filter data locally
+  let data: Data;
+  let filters: ShelfFilter[];
+
   return () => {
     const state = store.getState();
     const previousQuery = query;
@@ -16,8 +22,11 @@ export function createQueryListener(store: Store<State>) {
     const previousData = data;
     data = selectData(state);
 
+    const previousFilters = filters;
+    filters = selectFilters(state);
+
     // Check if either query or data has changed, need to submit a new query.
-    if (previousQuery !== query || previousData !== data) {
+    if (previousQuery !== query || previousData !== data || previousFilters !== filters) {
       dispatchQueries(store, query);
     }
   };
