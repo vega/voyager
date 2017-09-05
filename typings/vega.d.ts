93f5db38a60cedd19d9713c4ea22e5f2817d4498
starry <starrynight0225@gmail.com>
2017-09-05 11:37:06 -0700

    Use Voyager typings in Voyager project not vega-lite

---
+++ typings/vega.d.ts
@@ -0,0 +1,22 @@
+declare module 'vega' {
+  export const version: string;
+  export function parse(spec: any): any;
+  export class View {
+    constructor(runtime: any);
+    public logLevel(level: number): View;
+    public initialize(dom: Element | string): View;
+    public renderer(renderer: string): View;
+    public finalize(): void;
+    public hover(): View;
+    public run(): View;
+    public change(name: string, changeset: any): View;
+    public changeset(): any;
+    public data(name: string): object[];
+  }
+  export const Warn: number;
+  export const changeset: any;
+}
+
+declare module 'vega-event-selector' {
+  export function selector(selector: string, source: string): any[];
+}
