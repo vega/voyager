d924c7445123951c51fa61bb181b10f9d1b439c2
Shaheen Sharifian <shaheen.sharifian@gmail.com>
2017-11-20 21:22:48 -0800

    Add method signature tabbing

--- src/reducers/related-views.ts
+++ src/reducers/related-views.ts
@@ -2,8 +2,8 @@ import { Action } from '../actions/index';
 import { RELATED_VIEWS_HIDE_TOGGLE } from '../actions/related-views';
 import { DEFAULT_RELATED_VIEWS, RelatedViews } from '../models/related-views';
 
-export function relatedViewsReducer(relatedViewToggler: RelatedViews = DEFAULT_RELATED_VIEWS,
-                                    action: Action): RelatedViews {
+export function relatedViewsReducer(
+  relatedViewToggler: RelatedViews = DEFAULT_RELATED_VIEWS, action: Action): RelatedViews {
   const { isHidden } = relatedViewToggler;
   switch (action.type) {
     case RELATED_VIEWS_HIDE_TOGGLE: {
