1b9041be577b5598f0d87e7c6724f0e321da1cdc
Shaheen Sharifian <shaheen.sharifian@gmail.com>
2018-03-01 00:41:51 -0800

    Stop query compassQL when related-views tab hidden
    Add condition in QueryListener
    
    Add logic for case where related-views hidden before making query

--- src/store/listener.ts
+++ src/store/listener.ts
@@ -2,12 +2,14 @@ import {Query} from 'compassql/build/src/query/query';
 import {Store} from 'redux';
 import {Data} from 'vega-lite/build/src/data';
 import {State} from '../models/index';
+import {RelatedViews} from '../models/related-views';
 import {dispatchQueries} from '../queries/index';
-import {selectData, selectQuery} from '../selectors/index';
+import {selectData, selectQuery, selectRelatedViews} from '../selectors/index';
 
 export function createQueryListener(store: Store<State>) {
   let data: Data;
   let query: Query;
+  let relatedViews: RelatedViews;
   return () => {
     const state = store.getState();
     const previousQuery = query;
@@ -16,12 +18,16 @@ export function createQueryListener(store: Store<State>) {
     const previousData = data;
     data = selectData(state);
 
+    const previousRelatedViews = relatedViews;
+    relatedViews = selectRelatedViews(state);
+
+
     if (!data) {
       return;
     }
 
     // Check if either query or data has changed, need to submit a new query.
-    if (previousQuery !== query || previousData !== data) {
+    if (previousQuery !== query || previousData !== data || previousRelatedViews !== relatedViews) {
       dispatchQueries(store, query);
     }
   };
