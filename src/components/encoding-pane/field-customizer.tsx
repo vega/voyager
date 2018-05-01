149905646ab4ad9a50b3d9520361cb07616fdca5
Shaheen Sharifian <shaheen.sharifian@gmail.com>
2018-05-01 14:02:04 -0700

    Custom Encoding for Positional Channels (#789)
    * Add custom encoding for Positional Fields
    
    * Add factory for generating property-editor schema
    
    * Disjoint Schemas Option
    
    
    interim
    
    * Refactor prop-editor-schema
    
    Refactor any to interfaces.
    Create UiSchema from factory method
    Interim
    
    * Address PR comments. WIP on refactor getFormData method
    
    * Refactor form data & address PR comments
    
    * Address minor refactors and gitignore
    
    
    Add package-lock.json to gitignore
    
    * Add clickoutside to close fieldCustomizer
    
    Minor css edits

--- src/components/encoding-pane/field-customizer.tsx
+++ src/components/encoding-pane/field-customizer.tsx
@@ -1,46 +1,69 @@
-
 import * as React from 'react';
 import * as CSSModules from 'react-css-modules';
-import {ActionHandler} from '../../actions/redux-action';
-import {SpecEncodingAction} from '../../actions/shelf/spec';
-import {ShelfFieldDef, ShelfId} from '../../models/shelf/spec/encoding';
+import {Tab, TabList, TabPanel, Tabs} from 'react-tabs'; // eslint-disable-line
+import {ActionHandler} from '../../actions';
+import {SpecEncodingAction} from '../../actions/shelf';
+import {ShelfFieldDef, ShelfId} from '../../models/shelf/spec';
 import * as styles from './field-customizer.scss';
 import {PropertyEditor} from './property-editor';
+import {getFieldPropertyGroupIndex} from './property-editor-schema';
 
 export interface FieldCustomizerProps extends ActionHandler<SpecEncodingAction> {
   shelfId: ShelfId;
   fieldDef: ShelfFieldDef;
 }
 
+export interface CustomProp {
+  prop: string;
+  nestedProp?: string;
+}
 
 export class FieldCustomizerBase extends React.PureComponent<FieldCustomizerProps, {}> {
-
   public render() {
     const {shelfId, handleAction, fieldDef} = this.props;
+    const propertyGroupIndex = getFieldPropertyGroupIndex(shelfId, fieldDef);
+    const keys = Object.keys(propertyGroupIndex);
     return (
-      <div styleName='customizer-container'>
-        {this.customizableProps().map(customizableProp => {
-          const {prop, nestedProp} = customizableProp;
-          return (
-            <PropertyEditor
-              key={JSON.stringify({prop, nestedProp})}
-              prop={prop}
-              nestedProp={nestedProp}
-              shelfId={shelfId}
-              fieldDef={fieldDef}
-              handleAction={handleAction}
-            />
-          );
-        })}
+      <div styleName='field-customizer'>
+        <Tabs>
+          <TabList>
+            {
+              keys.map((encodingType, i) => {
+                return (
+                  <Tab key={i}>{encodingType}</Tab>
+                );
+              })
+            }
+          </TabList>
+          <div>
+            {
+              keys.map((encodingType, i) => {
+                const customProps: CustomProp[] = propertyGroupIndex[encodingType];
+                return (
+                  <TabPanel key={encodingType + i}>
+                    {
+                      customProps.map(customizableProp => {
+                        const {prop, nestedProp} = customizableProp;
+                        return (
+                          <PropertyEditor
+                            key={prop + '_' + nestedProp}
+                            prop={prop}
+                            nestedProp={nestedProp}
+                            shelfId={shelfId}
+                            fieldDef={fieldDef}
+                            handleAction={handleAction}
+                            propTab={encodingType}
+                          />
+                        );
+                      })
+                    }
+                  </TabPanel>
+                );
+              })}
+          </div>
+        </Tabs>
       </div>
     );
-  }
-
-  private customizableProps() {
-    return [{
-      prop: 'scale',
-      nestedProp: 'type'
-    }];
   }
 }
 
--- src/components/encoding-pane/property-editor.tsx
+++ src/components/encoding-pane/property-editor.tsx
@@ -1,37 +1,37 @@
 import * as React from 'react';
 import * as CSSModules from 'react-css-modules';
 import Form from 'react-jsonschema-form';
-import * as vlSchema from 'vega-lite/build/vega-lite-schema.json';
-import {ActionHandler} from '../../actions/redux-action';
-import {SPEC_FIELD_NESTED_PROP_CHANGE, SpecEncodingAction} from '../../actions/shelf/spec';
-import {ShelfFieldDef, ShelfId} from '../../models/shelf/spec/encoding';
+import {debounce} from 'throttle-debounce';
+import {ActionHandler} from '../../actions';
+import {SPEC_FIELD_NESTED_PROP_CHANGE, SPEC_FIELD_PROP_CHANGE, SpecEncodingAction} from '../../actions/shelf';
+import {ShelfFieldDef, ShelfId} from '../../models/shelf/spec';
+import {generateFormData, getPropertyEditorSchema} from './property-editor-schema';
 import * as styles from './property-editor.scss';
 
 export interface PropertyEditorProps extends ActionHandler<SpecEncodingAction> {
   prop: string;
   nestedProp: string;
+  propTab: string;
   shelfId: ShelfId;
   fieldDef: ShelfFieldDef;
 }
 
 export class PropertyEditorBase extends React.PureComponent<PropertyEditorProps, {}> {
+
   constructor(props: PropertyEditorProps) {
     super(props);
     this.changeFieldProperty = this.changeFieldProperty.bind(this);
+    this.changeFieldProperty = debounce(500, this.changeFieldProperty);
   }
 
   public render() {
-    const {prop, nestedProp, fieldDef} = this.props;
-    const uiSchema = {
-      "ui:title": `${prop} ${nestedProp}`,
-      "ui:placeholder": "auto",
-      "ui:emptyValue": "auto"
-    };
-    const formData = fieldDef.scale ? fieldDef.scale.type : 'auto';
+    const {prop, nestedProp, propTab, shelfId, fieldDef} = this.props;
+    const {schema, uiSchema} = getPropertyEditorSchema(prop, nestedProp, propTab);
+    const formData = generateFormData(shelfId, fieldDef);
     return (
       <div styleName="property-editor">
         <Form
-          schema={(vlSchema as any).definitions.ScaleType} // TODO don't use any
+          schema={schema}
           uiSchema={uiSchema}
           formData={formData}
           onChange={this.changeFieldProperty}
@@ -45,16 +45,27 @@ export class PropertyEditorBase extends React.PureComponent<PropertyEditorProps,
 
   protected changeFieldProperty(result: any) {
     const {prop, nestedProp, shelfId, handleAction} = this.props;
-    const value = result.formData;
-    handleAction({
-      type: SPEC_FIELD_NESTED_PROP_CHANGE,
-      payload: {
-        shelfId,
-        prop,
-        nestedProp,
-        value
-      }
-    });
+    const value = result.formData[Object.keys(result.formData)[0]];
+    if (nestedProp) {
+      handleAction({
+        type: SPEC_FIELD_NESTED_PROP_CHANGE,
+        payload: {
+          shelfId,
+          prop,
+          nestedProp,
+          value
+        }
+      });
+    } else {
+      handleAction({
+        type: SPEC_FIELD_PROP_CHANGE,
+        payload: {
+          shelfId,
+          prop,
+          value
+        }
+      });
+    }
   }
 }
 
