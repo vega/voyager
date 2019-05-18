176155884aa5d0db87b884fcd205c1f4bbfc2efd
Kanit Wongsuphasawat <kanitw@gmail.com>
2019-05-18 13:16:32 -0700

    Compact encoding pane prototype

--- src/components/app-root/index.tsx
+++ src/components/app-root/index.tsx
@@ -1,24 +1,22 @@
-
-import * as React from 'react';
-import {DragDropContext} from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
-import {connect} from 'react-redux';
-import {ClipLoader} from 'react-spinners';
-import * as SplitPane from 'react-split-pane';
-import {SPINNER_COLOR} from '../../constants';
-import {VoyagerConfig} from '../../models/config';
-import {Dataset} from '../../models/dataset';
-import {State} from '../../models/index';
-import {selectConfig} from '../../selectors';
-import {selectDataset} from '../../selectors/dataset';
-import '../app.scss';
-import {DataPane} from '../data-pane/index';
-import {EncodingPane} from '../encoding-pane/index';
-import {Footer} from '../footer/index';
-import {Header} from '../header/index';
-import {LoadData} from '../load-data-pane/index';
-import {LogPane} from '../log-pane/index';
-import {ViewPane} from '../view-pane/index';
+import * as React from "react";
+import { DragDropContext } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+import { connect } from "react-redux";
+import { ClipLoader } from "react-spinners";
+import * as SplitPane from "react-split-pane";
+import { SPINNER_COLOR } from "../../constants";
+import { VoyagerConfig } from "../../models/config";
+import { Dataset } from "../../models/dataset";
+import { State } from "../../models/index";
+import { selectConfig } from "../../selectors";
+import { selectDataset } from "../../selectors/dataset";
+import "../app.scss";
+import { CompactEncodingPane } from "../compact-encoding-pane/index";
+import { Footer } from "../footer/index";
+import { Header } from "../header/index";
+import { LoadData } from "../load-data-pane/index";
+import { LogPane } from "../log-pane/index";
+import { ViewPane } from "../view-pane/index";
 
 export interface AppRootProps {
   dataset: Dataset;
@@ -27,32 +25,60 @@ export interface AppRootProps {
 
 class AppRootBase extends React.PureComponent<AppRootProps, {}> {
   public render() {
-    const {dataset, config} = this.props;
-    const {hideHeader, hideFooter} = config;
+    const { dataset, config } = this.props;
+    const { hideHeader, hideFooter } = config;
     let bottomPane, footer;
     if (!dataset.isLoading) {
       if (!dataset.data) {
-        bottomPane = <LoadData/>;
+        bottomPane = <LoadData />;
       } else {
+        // bottomPane = (
+        //   <SplitPane
+        //     split="vertical"
+        //     defaultSize={200}
+        //     minSize={175}
+        //     maxSize={350}
+        //   >
+        //     <DataPane />
+        //     <SplitPane
+        //       split="vertical"
+        //       defaultSize={235}
+        //       minSize={200}
+        //       maxSize={350}
+        //     >
+        //       <EncodingPane />
+        //       <ViewPane />
+        //     </SplitPane>
+        //   </SplitPane>
+        // );
+
         bottomPane = (
-          <SplitPane split="vertical" defaultSize={200} minSize={175} maxSize={350}>
-            <DataPane/>
+          <SplitPane
+            split="vertical"
+            defaultSize={200}
+            minSize={175}
+            maxSize={350}
+          >
+            <CompactEncodingPane />
+            <ViewPane />
+            {/* <DataPane/>
             <SplitPane split="vertical" defaultSize={235} minSize={200} maxSize={350}>
               <EncodingPane/>
               <ViewPane/>
-            </SplitPane>
+            </SplitPane> */}
           </SplitPane>
         );
+
         if (!hideFooter) {
-          footer = <Footer/>;
+          footer = <Footer />;
         }
       }
     }
     return (
       <div className="voyager">
-        <LogPane/>
-        {!hideHeader && <Header/>}
-        <ClipLoader color={SPINNER_COLOR} loading={dataset.isLoading}/>
+        <LogPane />
+        {!hideHeader && <Header />}
+        <ClipLoader color={SPINNER_COLOR} loading={dataset.isLoading} />
         {bottomPane}
         {footer}
       </div>
@@ -60,12 +86,9 @@ class AppRootBase extends React.PureComponent<AppRootProps, {}> {
   }
 }
 
-export const AppRoot = connect(
-  (state: State) => {
-    return {
-      dataset: selectDataset(state),
-      config: selectConfig(state)
-    };
-  }
-)(DragDropContext(HTML5Backend)(AppRootBase));
-
+export const AppRoot = connect((state: State) => {
+  return {
+    dataset: selectDataset(state),
+    config: selectConfig(state)
+  };
+})(DragDropContext(HTML5Backend)(AppRootBase));
---
+++ src/components/compact-encoding-pane/index.tsx
@@ -0,0 +1,134 @@
+import {Schema} from 'compassql/build/src/schema';
+import * as React from 'react';
+import * as CSSModules from 'react-css-modules';
+import {connect} from 'react-redux';
+import {Channel} from 'vega-lite/build/src/channel';
+import {FieldOneOfPredicate, FieldRangePredicate} from 'vega-lite/build/src/predicate';
+import {FilterAction} from '../../actions';
+import {ActionHandler} from '../../actions/index';
+import {createDispatchHandler} from '../../actions/redux-action';
+import {ResultAsyncAction} from '../../actions/result';
+import {ShelfAction} from '../../actions/shelf/index';
+import {SPEC_FIELD_ADD} from '../../actions/shelf/spec';
+import {Dataset, State} from '../../models';
+import {ShelfUnitSpec} from '../../models';
+import {VoyagerConfig} from '../../models/config';
+import {ShelfFieldDef} from '../../models/shelf';
+import {selectConfig, selectDataset} from '../../selectors/';
+import {selectSchemaFieldDefs} from '../../selectors/dataset';
+import {selectFilters, selectShelfSpec} from '../../selectors/shelf';
+import {DataSelector} from '../data-selector';
+import {MarkPicker} from '../encoding-pane/mark-picker';
+import * as styles from './compact-encoding-pane.scss';
+
+export interface CompactEncodingPaneProps extends ActionHandler<ShelfAction | ResultAsyncAction | FilterAction> {
+  spec: ShelfUnitSpec;
+
+  filters: Array<FieldRangePredicate | FieldOneOfPredicate>;
+
+  schema: Schema;
+
+  fieldDefs: ShelfFieldDef[];
+
+  data: Dataset;
+  config: VoyagerConfig;
+}
+
+export class CompactEncodingPaneBase extends React.PureComponent<CompactEncodingPaneProps, {}> {
+  public render() {
+    const {name} = this.props.data;
+    const {showDataSourceSelector} = this.props.config;
+
+    const channels = ['x', 'y', 'size', 'color', 'shape', 'text', 'row', 'column'];
+
+    return (
+      <div className="pane" styleName="data-pane">
+        <h2 styleName="data-pane-title">Data</h2>
+        <div>
+          <span styleName="current-dataset">
+            <i className="fa fa-database" /> {name}
+          </span>
+          <span className="right">{showDataSourceSelector ? <DataSelector title="Change" /> : null}</span>
+        </div>
+        <div styleName="data-pane-section">
+          <div className="right">{this.markPicker()}</div>
+          <h3>Mark</h3>
+        </div>
+        <div styleName="data-pane-section">
+          <h3>Encoding</h3>
+          {channels.map(this.encodingShelf.bind(this))}
+        </div>
+      </div>
+    );
+  }
+
+  private onFieldChange(channel: Channel, field: string) {
+    const {schema} = this.props;
+
+    const type = schema.vlType(field);
+
+    console.log('field change', channel, field, type);
+
+    this.props.handleAction({
+      type: SPEC_FIELD_ADD,
+      payload: {
+        shelfId: {channel},
+        fieldDef: {
+          field,
+          type
+        },
+        replace: true
+      }
+    });
+  }
+
+  private encodingShelf(channel: Channel) {
+    const {spec, schema} = this.props;
+    const {encoding} = spec;
+
+    const options = schema.fieldNames().map(field => {
+      return (
+        <option key={field} value={field}>
+          {field}
+        </option>
+      );
+    });
+
+    const field = (encoding[channel] || {}).field;
+
+    const onChange = (event: any) => {
+      this.onFieldChange(channel, event.target.value);
+    };
+
+    return (
+      <div key={channel}>
+        <span>{channel}</span>
+
+        <select value={field} onChange={onChange}>
+          <option value={undefined}>-</option>
+          {options}
+        </select>
+      </div>
+    );
+  }
+
+  private markPicker() {
+    const {handleAction, spec} = this.props;
+    const {mark} = spec;
+    return <MarkPicker mark={mark} handleAction={handleAction} />;
+  }
+}
+
+export const CompactEncodingPane = connect(
+  (state: State) => {
+    return {
+      data: selectDataset(state),
+      config: selectConfig(state),
+      spec: selectShelfSpec(state),
+      filters: selectFilters(state),
+      schema: selectDataset(state).schema,
+      fieldDefs: selectSchemaFieldDefs(state)
+    };
+  },
+  createDispatchHandler<ShelfAction>()
+)(CSSModules(CompactEncodingPaneBase, styles));
